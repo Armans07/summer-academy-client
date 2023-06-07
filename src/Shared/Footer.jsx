568ab50007da2c45c3c59e011392258cfe13c9d0
Armans0701~ <sikder89098@gmail.com>
2023-06-07 14:18:41 +0600

    added route

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
                 <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                     <div className="md:flex md:justify-between">
                         <div className="mb-6 md:mb-0">
-                                <img src={logo} className="h-20 mr-3" alt="FlowBite Logo" />
-                                <span className="self-center text-1xl font-semibold whitespace-nowrap dark:text-white">Summer champ</span>
+                            <img src={logo} className="h-20 mr-3" alt="FlowBite Logo" />
+                            <span className="self-center text-1xl font-semibold whitespace-nowrap dark:text-white">Summer champ</span>
                         </div>
                         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                             <div>
